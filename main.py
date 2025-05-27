41b6126aa6084067b9eb0ab013636c85f362bd5f
imarkovic2002 <markovici089@gmail.com>
2025-05-27 18:19:12 +0200

    dodane rute za filtriranje prosjecne temperature po drzavi i po gradu i godini

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import pandas as pd
 
 weather_df = pd.read_csv("weather_data.csv", parse_dates=["datum"])
@@ -11,6 +11,23 @@ def main_root():
 
 @app.get("/proba")
 def get_random_data():
+    if weather_df.empty:
+        raise HTTPException(status_code=404, detail="CSV podaci nisu dostupni")
+    
     sample = weather_df.sort_values("datum").head(5)
     return sample.to_dict(orient="records")
 
+@app.get("/average_country")
+def average_per_country(drzava:str):
+    df = weather_df[weather_df["država"] == drzava]
+    if df.empty:
+        raise HTTPException(status_code=404, detail="Nema podataka za tu državu.")
+    return {
+        "država":drzava,
+        "prosječna temperatura": round(df["temperatura"].mean(), 2)
+    }
+
+@app.get("/filter")
+def filter_data(grad:str, godina:int):
+    df = weather_df [(weather_df["grad"].str.lower() == grad.lower()) & (weather_df["datum"].dt.year == godina) ]
+    return df.head(10).to_dict(orient="records")
